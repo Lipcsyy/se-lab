b67222eb12f4554c887423ad03f1e4971734b802
Lipcsyy <lipcsey.magyar.marton@gmail.com>
2023-09-27 15:25:41 +0200

    little change on the B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -87,6 +87,7 @@ public class GT4500 implements SpaceShip {
 			firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
 		}
 		else {
+			boolean a = false;
 			firingSuccess = false;
 		}
 
